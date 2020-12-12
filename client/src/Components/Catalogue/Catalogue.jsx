f36d9152d1bf356261ecfe6b0c64966b00b62e5f
unknown <santiagofernandezdelasierra@gmail.com>
2020-12-11 21:31:38 -0300

    Categorias Funcionales

--- client/src/Components/Catalogue/Catalogue.jsx
+++ client/src/Components/Catalogue/Catalogue.jsx
@@ -3,23 +3,23 @@ import ProductCard from "../ProductCard/ProductCard.jsx";
 import "./Catalogue.modules.css";
 import Sidebar from "../sidebar/sidebar.jsx";
 import { connect } from "react-redux";
-import { getProductsList } from "../../actions"
+import { getProductsList, getCategoryList } from "../../actions"
 
 
 function Catalogue(props) {
-
+  props.getCategoryList()
   if (props.allProducts.length === 0) {
   
     props.getProductsList()
   }
 
   if (props.allProducts.length !== 0) {
-   
+   console.log('CATALOGO',props.allProducts)
     return (
       <div className="Catalogue__container">
         <Sidebar></Sidebar>
         <div className="Catalogue__Div">
-          {props.allProducts[0].data.map((product, index) => {
+          {props.allProducts[0].map((product, index) => {
             return <ProductCard data={product} key={index} />;
           })}
         </div>
@@ -38,4 +38,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps,{getProductsList})(Catalogue);
+export default connect(mapStateToProps,{getProductsList,getCategoryList})(Catalogue);
