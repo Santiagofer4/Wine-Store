883b46de81a19b2fa72b88e248b065aeff005b93
flaviogragnolati <flavio.gragnolat@gmail.com>
2020-12-14 14:42:50 -0300

    Bug fix de delete de select vacio

--- client/src/Components/Admin/LoadPorduct/LoadProduct.jsx
+++ client/src/Components/Admin/LoadPorduct/LoadProduct.jsx
@@ -331,7 +331,6 @@ const LoadProduct = (props) => {
                     options={tasteList}
                     id="delete3"
                     value={initialValues.taste3}
-                    onChange={(e) => handleOnClickSelect(e)}
                   />
                 ) : (
                   <FormField
