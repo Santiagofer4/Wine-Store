c49b7bde8660917cb56b925c23d45e16183249b1
MG <marcosg1996@hotmail.com.ar>
2020-12-15 20:02:42 -0300

    ruta de orders y relacion con user

--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -1,14 +1,43 @@
 const server = require('express').Router();
-
+const {Order,User} = require('../db.js');
 server.get('/', (req, res, next) => {
-    console.log('GET a ORDERS');
+  // debe devolver todas las ordenes si no recibe status
 
-    Order.findAll()
-    .then((order) => {
-     return res.status(200).send(order);
-    })
-    .catch(next);
+  const {status} = req.query;
+    console.log('GET a ORDERS');
+    if(!status){
+      Order.findAll()
+      .then((order) => {
+       return res.status(200).send(order);
+      })
+      .catch(next);
+    }else{
+      Order.findAll({where:{ status}})
+      .then((list)=>{
+        console.log('respuesta')
+        res.json(list)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    }
   });
+
+  server.post('/',(req,res)=>{
+    const{status,total,userId,}=req.body;
+    Order.create({
+      status,
+      total,
+    })
+    .then((order)=>{     
+      return order.setUser(userId)
+    })
+    .then(() =>{
+      res.send('se agrego una nueva orden')
+
+    })
+  })
+
 
 
 module.exports = server
