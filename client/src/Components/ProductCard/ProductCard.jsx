ec6fb8aaf5aa4cb2b196efda1ca0084bf0d6842a
MG <marcosg1996@hotmail.com.ar>
2020-12-23 12:12:43 -0300

    productCard migrado

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -5,14 +5,9 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import './ProductCard.modules.css';
-import { connect , useDispatch} from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { wineDetails} from '../../slices/productDetailSlice';
 import { addToCart, postProductsCar} from '../../slices/productsCartSlice'
-import {
-  setProductDetail,
-  addProductCart,
-  putProductCart,
-} from '../../actions';
 import { useHistory } from 'react-router-dom';
 
 // Recibe props con Products.info
@@ -23,41 +18,17 @@ function ProductCard(props) {
   const history = useHistory();
 
   const detailClickHandler = () => {
-    // console.log('CLICK CARD', props.data);
     dispatch(wineDetails(props.data))
-    // props.setProductDetail(props.data);
     history.push(`/product/${id}`);
   };
+
   // refactorizar esta funcion
   function handlerProductToCart(userId, id, price) {
     let productDetail ={image, name, price, id, stock, quantity:1}
       dispatch(addToCart({userId,productDetail}))
       let e = productDetail;
       dispatch(postProductsCar({e,userId}))
-    // if (props.productsCart[0] !== undefined) {
-    //   //entro a la funcion
-    //   if (props.productsCart[0].orderLines !== undefined) {
-    //     //cuando el estado tiene algo
-    //     if (props.productsCart[0].orderLines.length > 0) {
-    //       //cuando el estado tiene un orderLine y tiene datos
-    //       let products = props.productsCart[0].orderLines;
-    //       for (let i = 0; i < products.length; i++) {
-    //         if (products[i].productId === id) {
-    //           if (products[i].quantity <= products[i].product.stock) {
-    //             props.putProductCart(1, id, products[i].quantity + 1);
-    //             return (i = products.length);
-    //           }
-    //         } else if (i === products.length - 1) {
-    //           props.addProductCart(userId, id, price);
-    //         }
-    //       }
-    //     } else {
-    //       props.addProductCart(userId, id, price); //cuando la orderLine no tiene datos
-    //     }
-    //   }
-    // } else {
-    //   props.addProductCart(userId, id, price); //cuando el estado es undefined/ esta vacio
-    // }
+ 
   }
 
   return (
@@ -85,15 +56,11 @@ function ProductCard(props) {
               <h3>No hay STOCK</h3>
             ) : (
               <Button
-                id="Button__Buy"
-                onClick={() => {
-                  handlerProductToCart(1, id, price);
-                }}
+                id="Button__Buy" onClick={() => { handlerProductToCart(1, id, price);}}
               >
                 Comprar
               </Button>
             )}
-
             <Button id="Button__Info" onClick={detailClickHandler}>
               +
             </Button>
@@ -103,13 +70,7 @@ function ProductCard(props) {
     </Card>
   );
 }
-const mapStateToProps = (state) => ({
-  productsCart: state.productReducers.productsCart,
-});
-export default connect(mapStateToProps, {
-  setProductDetail,
-  addProductCart,
-  putProductCart,
-})(ProductCard);
+export default ProductCard;
+
 // Este componente es una tarjeta donde tiene la información básica del Producto.
 // Nos va a servir para ser usado en el componente Catálogo.
