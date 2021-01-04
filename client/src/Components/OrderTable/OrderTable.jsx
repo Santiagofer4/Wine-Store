151fc30e7b00f3fca7e3c460398e32aaef9005ba
flaviogragnolati <flavio.gragnolat@gmail.com>
2021-01-03 22:47:41 -0300

    Se agrega loading status al componente OrderTable

--- client/src/Components/OrderTable/OrderTable.jsx
+++ client/src/Components/OrderTable/OrderTable.jsx
@@ -5,6 +5,7 @@ import './OrderTable.modules.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOrderTable } from '../../slices/orderTableSlice';
 import { allOrderSelector, allOrderStatusSelector } from '../../selectors';
+import { CircularProgress, Button } from '@material-ui/core';
 
 // Esta tabla es para el admin.
 // Tiene que mostrar todas las ordenes de todos los usuarios.
@@ -13,11 +14,60 @@ function OrderTable() {
   const dispatch = useDispatch();
   const orderTable = useSelector(allOrderSelector);
   const status = useSelector(allOrderStatusSelector);
-
+  let content;
   useEffect(() => {
     dispatch(getOrderTable());
   }, [dispatch]);
 
+  const handleRetry = () => {
+    //func para reintentar y forzar refresh
+    // history.push(props.location.pathname);
+    window.location.reload();
+    return false;
+  };
+
+  if (status === 'loading') {
+    content = (
+      <>
+        <h2>Cargando...</h2>
+        <CircularProgress />
+      </>
+    );
+  } else if (status === 'succeded') {
+    content = orderTable.map((order) => {
+      return (
+        <>
+          <li key={order.id} className="OrderTable__li">
+            <div className="OrderTable__Text">{order.id}</div>
+            <div className="OrderTable__Text">
+              {Math.ceil((total(order.orderLines) * 121) / 100)}
+            </div>
+            <div className="OrderTable__Text">{order.status}</div>
+            <div className="OrderTable__Text">{order.userId}</div>
+            <div className="OrderTable__Text">{sliceTime(order.updatedAt)}</div>
+            <div className="OrderTable__Text">
+              {' '}
+              <button
+                onClick={() => {
+                  hide(order.id);
+                }}
+              >
+                D
+              </button>
+            </div>
+          </li>
+          <OrderDetail id={order.id} data={order.orderLines}></OrderDetail>
+        </>
+      );
+    });
+  } else if (status === 'failed') {
+    content = (
+      <>
+        <h3>Ha ocurrido un error</h3>
+        <Button onClick={handleRetry}>Reintentar</Button>
+      </>
+    );
+  }
   return (
     <div className="OrderTable__Container">
       <li key={orderTable.id} className="OrderTable__li">
@@ -29,35 +79,7 @@ function OrderTable() {
         <div className="OrderTable__index">Detalle</div>
       </li>
 
-      {status === 'succeded' &&
-        orderTable.map((order) => {
-          return (
-            <>
-              <li key={order.id} className="OrderTable__li">
-                <div className="OrderTable__Text">{order.id}</div>
-                <div className="OrderTable__Text">
-                  {Math.ceil((total(order.orderLines) * 121) / 100)}
-                </div>
-                <div className="OrderTable__Text">{order.status}</div>
-                <div className="OrderTable__Text">{order.userId}</div>
-                <div className="OrderTable__Text">
-                  {sliceTime(order.updatedAt)}
-                </div>
-                <div className="OrderTable__Text">
-                  {' '}
-                  <button
-                    onClick={() => {
-                      hide(order.id);
-                    }}
-                  >
-                    D
-                  </button>
-                </div>
-              </li>
-              <OrderDetail id={order.id} data={order.orderLines}></OrderDetail>
-            </>
-          );
-        })}
+      {content}
     </div>
   );
 }
