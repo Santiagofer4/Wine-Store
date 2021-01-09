a6644a045529b8cd22620937d972786ad20b4138
Tanito <ulises.d83@gmail.com>
2021-01-08 22:56:25 -0300

    fixed logout message

--- client/src/Components/Contenido/Logout.jsx
+++ client/src/Components/Contenido/Logout.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '@material-ui/core';
 import axios from 'axios';
 import tokenManager from '../utils/tokenManager';
 import { isLogged } from '../utils/index';
 
+
 function Logout() {
+  const [logged, setLogin] = useState(false);
   //   const logout = async () => {
   //     console.log('LOGGIN OUT');
   //     const resp = await axios.get('http://localhost:3000/auth/logout');
@@ -14,10 +16,15 @@ function Logout() {
   //     }
   //   };
   //   useEffect(logout(), []);
+  useEffect(() => {
+    let logged = isLogged();
+
+    
+  }, []);
 
   return (
     <>
-      {isLogged() ? (
+      {logged ? (
         <Container className="Main__content">
           <h1 Post__registerLinks> SEGUIS LOGEADO!!!!</h1>
         </Container>
